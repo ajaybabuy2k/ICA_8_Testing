e824dc60f76fdf9bf91f3e70acfe1428f52df1fd
ajaybabuy2k <ayadava1@asu.edu>
2022-10-27 23:09:39 -0700

    Testing count Urinels testcase 8

--- ICA8/src/test/java/urinalsTest.java
+++ ICA8/src/test/java/urinalsTest.java
@@ -11,31 +11,36 @@ class urinalsTest {
     @Test
     void goodString() {
 
-        System.out.println("====== Ajay Babu Yadavalli == TEST ONE EXECUTED =======");
+        System.out.println("====== Ajay Babu Yadavalli == TEST TWO EXECUTED =======");
         System.out.println("Executed properly and passed assertion with 10101");
         assertEquals(true,urinals.goodString("10101"));
 
-        System.out.println("====== Ajay Babu Yadavalli == TEST TWO EXECUTED =======");
+        System.out.println("====== Ajay Babu Yadavalli == TEST THREE EXECUTED =======");
         System.out.println("Executed properly and passed assertion with 1001");
         assertEquals(true,urinals.goodString("1001"));
 
-        System.out.println("====== Ajay Babu Yadavalli == TEST THREE EXECUTED =======");
+        System.out.println("====== Ajay Babu Yadavalli == TEST FOUR EXECUTED =======");
         System.out.println("Executed properly and passed assertion with 00000");
         assertEquals(true,urinals.goodString("00000"));
 
-        System.out.println("====== Ajay Babu Yadavalli == TEST FOUR EXECUTED =======");
+        System.out.println("====== Ajay Babu Yadavalli == TEST FIVE EXECUTED =======");
         System.out.println("Executed properly and passed assertion with 0000");
         assertEquals(true,urinals.goodString("0000"));
 
-        System.out.println("====== Ajay Babu Yadavalli == TEST FIVE EXECUTED =======");
+        System.out.println("====== Ajay Babu Yadavalli == TEST SIX EXECUTED =======");
         System.out.println("Executed properly and passed assertion with 01000");
         assertEquals(true,urinals.goodString("01000"));
 
-        System.out.println("====== Ajay Babu Yadavalli == TEST SIX EXECUTED =======");
+        System.out.println("====== Ajay Babu Yadavalli == TEST SEVEN EXECUTED =======");
         System.out.println("Executed properly and passed assertion with 011");
         assertEquals(false,urinals.goodString("011"));
 
+    }
 
+    @Test
+    public void countUrinals() {
+        System.out.println("======Ajay Babu Yadavalli == TEST EIGHT EXECUTED =======");
+        assertEquals(1,urinals.countUrinals("01000"));
     }
 
 }
