67f6ecd0a8e4bbddc0579238863a7bdd121a387f
ajaybabuy2k <ayadava1@asu.edu>
2022-10-27 23:03:33 -0700

    updated the urinels file with countUrinals

--- ICA8/src/main/java/urinals.java
+++ ICA8/src/main/java/urinals.java
@@ -20,7 +20,8 @@ public class urinals {
 
         else if(verifyGoodString==True)
         {
-            System.out.println("Valid Urinals Positions and Verified");
+            int urinalsCount=countUrinals(returnedString);
+            System.out.println(urinalsCount);
         }
 
     }
@@ -42,11 +43,88 @@ public class urinals {
     }
 
 
+    public static int countUrinals(String inputString)
+    {
+        int[]arrStringInput=new int[inputString.length()];
+        for(int iterator=0;iterator<arrStringInput.length;iterator++)
+        {
+            arrStringInput[iterator]=0;
+        }
+        for(int iterator=0;iterator<inputString.length();iterator++)
+        {
+            if(iterator==0)
+            {
+                if(inputString.charAt(iterator)=='0')
+                {
+                    if(iterator+1<inputString.length()) {
+
+                        if (inputString.charAt(iterator + 1) == '1') {
+                            arrStringInput[iterator] = 0;
+                        }
+
+                        else if(inputString.charAt(iterator + 1) != '1'){
+                            arrStringInput[iterator] = 1;
+                        }
+                    }
+                }
+                else if(iterator!=0) {
+
+                    arrStringInput[iterator]=0;
+
+                }
+            }
+            else if(iterator==1)
+            {
+                if(inputString.charAt(iterator)=='1') {
+                    arrStringInput[iterator] = arrStringInput[iterator - 1];
+                }
+                else if(inputString.charAt(iterator)!='1')
+                {
+                    if(inputString.charAt(iterator-1)=='1')
+                    {
+                        arrStringInput[iterator]=arrStringInput[iterator-1];
+                    }
+                    else if(iterator+1<inputString.length() && inputString.charAt(iterator+1)=='1')
+                    {
+                        arrStringInput[iterator]=arrStringInput[iterator-1];
+                    }
+                    else
+                    {
+                        arrStringInput[iterator]=1;
+                    }
+                }
+            }
+            else {
+                arrStringInput[iterator]=arrStringInput[iterator-1];
+                if(inputString.charAt(iterator)=='1') {
+                    arrStringInput[iterator] = arrStringInput[iterator - 1];
+                }
+                else
+                {
+                    if(inputString.charAt(iterator-1)=='1')
+                    {
+                        arrStringInput[iterator]=arrStringInput[iterator-1];
+                    }
+                    else if(iterator+1<inputString.length() && inputString.charAt(iterator+1)=='1')
+                    {
+                        arrStringInput[iterator]=arrStringInput[iterator-1];
+                    }
+                    else
+                    {
+                        arrStringInput[iterator]=Math.max(arrStringInput[iterator],1+arrStringInput[iterator-2]);
+                    }
+                }
+            }
+        }
+        return arrStringInput[inputString.length()-1];
+    }
+
+
     public static String getString(){
-        Scanner sc=new Scanner(System.in);
-        System.out.println("Enter the string:");
-        String str=sc.next();
-        return str;
+        Scanner scannerObj=new Scanner(System.in);
+        System.out.println("Enter the input string:");
+        String inputStr=scannerObj.next();
+        return inputStr;
     }
 
 }
